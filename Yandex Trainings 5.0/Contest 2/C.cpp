c55eeb716fa9870bbeaea17e5068122ca3aaff29
PurreCoder <113393847+PurreCoder@users.noreply.github.com>
2024-03-29 23:00:15 +0400

    Create C.cpp

---
+++ Yandex Trainings 5.0/Contest 2/C.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <set>
+#include <functional>
+#include <string>
+#include <stack>
+#include <iomanip>
+#include "inttypes.h"
+#include <sstream>
+#include <climits>
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define mp(x, y) make_pair(x, y)
+
+using ld = long double;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+
+template <typename T>
+istream& operator>>(istream& in, vector <T>& arr)
+{
+    for (T& i : arr) in >> i;
+    return in;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi l(n);
+    cin >> l;
+
+    int mx = *max_element(all(l));
+    int sum = accumulate(all(l), 0) - mx;
+    cout << (mx - sum > 0 ? mx - sum : mx + sum);
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+        //cout << '\n';
+    }
+    return 0;
+}
