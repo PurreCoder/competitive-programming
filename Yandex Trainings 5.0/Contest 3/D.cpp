f4ec9af380d97fd4edd025ea403068b21220aaa9
PurreCoder <113393847+PurreCoder@users.noreply.github.com>
2024-03-29 23:08:08 +0400

    Create D.cpp

---
+++ Yandex Trainings 5.0/Contest 3/D.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <set>
+#include <functional>
+#include <string>
+#include <stack>
+#include <iomanip>
+#include "inttypes.h"
+#include <sstream>
+#include <climits>
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define mp(x, y) make_pair(x, y)
+
+using ld = long double;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+
+template <typename T>
+istream& operator>>(istream& in, vector<T>& arr)
+{
+    for (T& i : arr) in >> i;
+    return in;
+}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    unordered_map<int, int> last_seen;
+    for (int i = 1; i <= n; ++i)
+    {
+        int num;
+        cin >> num;
+        int prev = last_seen[num];
+        if (prev != 0 && i - prev <= k)
+        {
+            cout << "YES";
+            return;
+        }
+        last_seen[num] = i;
+    }
+    cout << "NO";
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+        //cout << '\n';
+    }
+    return 0;
+}
