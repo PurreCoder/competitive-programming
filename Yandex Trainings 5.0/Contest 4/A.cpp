ea5980240c0231a2a75eac17333d0e31af1944ba
PurreCoder <113393847+PurreCoder@users.noreply.github.com>
2024-03-29 23:10:26 +0400

    Create A.cpp

---
+++ Yandex Trainings 5.0/Contest 4/A.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <string>
+#include <stack>
+#include <iomanip>
+#include "inttypes.h"
+#include <sstream>
+#include <climits>
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define mp(x, y) make_pair(x, y)
+
+using ld = long double;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+
+template <typename T>
+istream& operator>>(istream& in, vector<T>& arr)
+{
+    for (T& i : arr) in >> i;
+    return in;
+}
+
+void solve()
+{   
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    sort(all(a));
+
+    int k;
+    cin >> k;
+    while (k--)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << upper_bound(all(a), r) - lower_bound(all(a), l) << ' ';
+    }
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+        //cout << '\n';
+    }
+    return 0;
+}
